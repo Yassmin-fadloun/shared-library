47b3b3db6136ac4c53ca4bb9b0f504a40a485371
Yassmin-fadloun <88232979+Yassmin-fadloun@users.noreply.github.com>
2025-07-31 03:50:18 +0300

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,16 +1,10 @@
-def call(String imageName, String imageTag) {
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', 
-                                       usernameVariable: 'DOCKER_USER', 
-                                       passwordVariable: 'DOCKER_PASS')]) {
-
+def call(String imageTag) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-creds',
+                                      usernameVariable: 'DOCKER_USER',
+                                      passwordVariable: 'DOCKER_PASS')]) {
         sh """
-            echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin docker.io
-
-            # Tag the image with full Docker Hub path
-            docker tag ${imageName}:${imageTag} docker.io/${DOCKER_USER}/${imageName}:${imageTag}
-
-            # Push to Docker Hub
-            docker push docker.io/${DOCKER_USER}/${imageName}:${imageTag}
+            echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+            docker push yassminfadl0un/your-app:${imageTag}
         """
     }
 }
