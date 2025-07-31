ae0d798abdab88b3dd8c3d6db2ab3e7028664f2b
Yassmin-fadloun <88232979+Yassmin-fadloun@users.noreply.github.com>
2025-07-31 05:42:30 +0300

    Update pushManifests.groovy

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -1,24 +1,17 @@
 def call() {
     stage('Push Manifests ') {
-        withCredentials([usernamePassword(
-            credentialsId: 'docker-hub-creds', 
-            usernameVariable: 'GIT_USER', 
-            passwordVariable: 'GIT_TOKEN'
-        )]) {
+        // Use Secret Text for GitHub token
+        withCredentials([string(credentialsId: 'github-pat', variable: 'GIT_TOKEN')]) {
             sh '''
                 git config user.name "Jenkins"
                 git config user.email "jenkins@example.com"
-                git remote set-url origin https://${GIT_USER}:${GIT_TOKEN}@github.com/Yassmin-fadloun/shared-library.git
+                git remote set-url origin https://x-access-token:${GIT_TOKEN}@github.com/Yassmin-fadloun/shared-library.git
                 git fetch origin
                 git checkout -B main origin/main
                 git add -A
                 git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
                 git push origin main
             '''
-
         }
     }
 }
-
-
-
