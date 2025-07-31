a51e014a7d4a3d864fb7daabd5173b7eb2a42358
Yassmin-fadloun <88232979+Yassmin-fadloun@users.noreply.github.com>
2025-07-31 05:13:37 +0300

    Update scanImage.groovy

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -1,7 +1,7 @@
 def call() {
     stage('Scan Image ') {
         echo ' Scanning Docker image...'
-        withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+        withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh """
         pwd
         trivy image $DOCKER_USER/your-app:$BUILD_NUMBER
