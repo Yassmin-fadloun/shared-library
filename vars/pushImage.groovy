eaa0c3549ca641194c3c6b27dbb19a21898c9aae
Yassmin-fadloun <88232979+Yassmin-fadloun@users.noreply.github.com>
2025-07-31 04:37:17 +0300

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,12 +1,12 @@
 def call() {
-    script {
-        withCredentials([usernamePassword(
-            credentialsId: 'docker-hub-creds',
-            usernameVariable: 'DOCKER_USER',
-            passwordVariable: 'DOCKER_PASS'
-        )]) {
-            sh "echo ${DOCKER_PASS} | docker login -u ${DOCKER_USER} --password-stdin"
-            sh "docker push yassminfadloun/your-app:${env.BUILD_NUMBER}"
+    stage('Push Image ') {
+        echo 'Pushing Docker image to registry...'
+        withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+            sh """
+                echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+                docker tag yassminfadloun/your-app:$BUILD_NUMBER $DOCKER_USER/your-app:$BUILD_NUMBER
+                docker push $DOCKER_USER/your-app:$BUILD_NUMBER
+            """
         }
     }
 }
