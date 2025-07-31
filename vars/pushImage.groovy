e7f3d21d6830b9e6601e71c3cf1f18fb744e8bae
Yassmin-fadloun <88232979+Yassmin-fadloun@users.noreply.github.com>
2025-07-31 05:23:46 +0300

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -4,8 +4,8 @@ def call() {
         withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
             sh """
                 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-                docker tag yassminfadloun/your-app:$BUILD_NUMBER $DOCKER_USER/your-app:$BUILD_NUMBER
-                docker push $DOCKER_USER/your-app:$BUILD_NUMBER
+                docker tag yassminfadloun/your-app:$BUILD_NUMBER yassminfadloun/your-app:$BUILD_NUMBER
+                docker push yassminfadloun/your-app:$BUILD_NUMBER
             """
         }
     }
