fa23e488e5bf06cba93a3578455a11619e925596
Yassmin-fadloun <88232979+Yassmin-fadloun@users.noreply.github.com>
2025-07-31 04:43:17 +0300

    Update updateManifests.groovy

--- vars/updateManifests.groovy
+++ vars/updateManifests.groovy
@@ -1,7 +1,10 @@
 def call() {
-    script {
+    stage('Update Manifests ') {
+        echo 'Updating Kubernetes manifests (YAML files) using sed...'
         sh """
-            sed -i 's|image: .*|image: yassminfadloun/your-app:${env.BUILD_NUMBER}|g' K8s/deployment.yaml
+            grep 'image:' K8s/deployment.yaml || echo 'No image line found!'
+            cd $WORKSPACE
+            sed -i "s|image:.*|image: yassminfadloun/your-app:$BUILD_NUMBER|" K8s/deployment.yaml
         """
     }
 }
