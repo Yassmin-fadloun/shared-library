6bec69bf9f256c736ccf337f4f1ce599fabbb009
Yassmin-fadloun <88232979+Yassmin-fadloun@users.noreply.github.com>
2025-07-31 04:21:50 +0300

    Update scanImage.groovy

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -1,5 +1,5 @@
 def call() {
     script {
-        sh "trivy image --severity CRITICAL --exit-code 1 yassminfadl0un/your-app:${env.BUILD_NUMBER}"
+       sh "trivy image --severity CRITICAL --exit-code 0 ${env.DOCKER_IMAGE}"
     }
 }
